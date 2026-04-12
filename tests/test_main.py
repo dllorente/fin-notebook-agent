0c9264278555c789485f21b1a61fd13c38d77bb1
David Llorente <davidllorenteraposo@gmail.com>
2026-04-12 18:37:19 +0200

    add initial main test

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_app_exists():
+    assert app is not None
+    
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
